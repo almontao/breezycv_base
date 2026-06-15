1b6a65a92d288dd553f4aabb99421fbc1ad5c8ea
almontao <33377248+almontao@users.noreply.github.com>
2026-06-14 21:46:53 -0500

    Remove bad transitions

--- js/animating.js
+++ js/animating.js
@@ -176,9 +176,9 @@ var PageTransitions = (function ($, options) {
         //}
         if (!($pageTrigger.attr('data-animation'))) {
             
-            // var animNumber = 1; // DESCOMENTA ESTA LÍNEA PARA USAR UNA FIJA (Opciones seguras: del 1 al 12, y del 17 al 20)
+            // var animNumber = 1; // DESCOMENTA ESTA LÍNEA PARA USAR UNA FIJA (Opciones seguras: 1, 4, 6, 8, 10, 11, 12, 20)
             
-            var seguras = [1, 2, 4, 5, 6, 7, 8, 10, 11, 12, 17, 20];
+            var seguras = [1, 4, 6, 8, 10, 11, 12, 20];
             var animNumber = seguras[Math.floor(Math.random() * seguras.length)];
             
             $pageTrigger.data('animation',animNumber);
